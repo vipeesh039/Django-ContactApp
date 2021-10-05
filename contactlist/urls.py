a5188a93a847c5df35a2308e4d6b723009e1059e
vipeesh039 <vipeesh039@gmail.com>
2021-10-05 17:48:54 +0530

    contact-App

---
+++ contactlist/urls.py
@@ -0,0 +1,28 @@
+"""contact URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('add',views.addcontact,name="add"),
+    path('profile/<str:pk>',views.contactProfile,name="profile"),
+    path('edit/<str:pk>',views.editContact,name="edit"),
+    path('delete/<str:pk>',views.deleteContact,name="delete"),
+
+]
---
+++ contactlist/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from .models import Contact
+
+# Create your views here.
+def index(request):
+    contacts=Contact.objects.all()
+    search_input=request.GET.get('search-area')
+    if search_input:
+        contacts=Contact.objects.filter(full_name__icontains=search_input)
+    else:
+        contacts=Contact.objects.all()
+        search_input=''
+    return render(request,"index.html",{'contacts':contacts, 'search_input':search_input})
+
+def addcontact(request):
+    if request.method== 'POST':
+        new_contact= Contact(
+            full_name=request.POST["fullname"],
+            relationship=request.POST["relationship"],
+            email=request.POST["email"],
+            phone_number=request.POST["phone-number"],
+            address=request.POST["address"]
+        )
+        new_contact.save()
+        return redirect("/")
+    return render(request,"new.html")
+
+def contactProfile(request,pk):
+    contact=Contact.objects.get(id=pk)
+    return render(request,'contact-profile.html',{'contact':contact})
+
+def editContact(request,pk):
+    contact = Contact.objects.get(id=pk)
+    if request.method=='POST':
+        contact.full_name=request.POST["full_name"]
+        contact.relationship = request.POST["relationship"]
+        contact.phone_number = request.POST["phone_number"]
+        contact.email = request.POST["email"]
+        contact.address = request.POST["address"]
+        contact.save()
+        return redirect('/profile/'+contact.id)
+    return render(request,'edit.html',{'contact':contact})
+
+def deleteContact(request,pk):
+    contact=Contact.objects.get(id=pk)
+    if request.method== 'POST':
+        contact.delete()
+        return redirect('/')
+    return render(request,'delete.html',{'contact':contact})
+
